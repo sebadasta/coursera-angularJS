5603f1b714b1ae2fcc64d12d73d184e307aac237
Jes <jes@MacBook-Air-de-Jes.local>
2016-11-24 18:10:27 +0000

    module1 1 - Assigment

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -5,10 +5,60 @@ angular.module('LunchCheck', [])
 .controller('LunchCheckController', LunchCheckController);
 
 LunchCheckController.$inject = ['$scope'];
+
 function LunchCheckController($scope) {
-  $scope.name = "Yaakov";
+
+
+$scope.foodList = "" ;
+var foodArray = [];
+$scope.foodCount = 0;
+$scope.message = "";
+
+
+$scope.CheckLunch = function () {
+
+     $scope.foodCount = 0;
+
+     foodArray = $scope.foodList.split(',');
+
+
+  for (var i = 0; i < foodArray.length; i++) {
+
+  if (foodArray[i].length <1) {
+
+    //Do nothing
+
+         } else {
+
+    $scope.foodCount = $scope.foodCount + 1;
+
+   }
+
+}
+
+if ($scope.foodList.length == 0) {
+
+  $scope.message = "Please enter data first";
+
+    } else {
+
+      if ($scope.foodCount >3) {
+
+          $scope.message = "Too much!";
+
+            } else {
+
+              $scope.message = "Enjoy!";
+
+      }
+
+}
+
+
+};
 
 
 }
+
 
 })();
